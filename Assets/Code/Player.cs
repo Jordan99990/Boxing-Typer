e2dc02e69a0b5860a980209cf773247315055122
Podolac Eduard <eduardpodolac@gmail.com>
2024-06-08 19:14:24 +0300

    prepared Hud component in Player script

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -8,11 +10,18 @@ public class Player : MonoBehaviour
 
     private Animator PlayerAnimator;
     private Animator EnemyAnimator;
+
+    public GameObject TextBackground;
+    public Text TextComponent;
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerAnimator = this.GetComponent<Animator>();
         EnemyAnimator = Enemy.GetComponent<Animator>();
+
+        TextBackground.SetActive(false);
+        TextComponent.text = "";
     }
 
     // Update is called once per frame
